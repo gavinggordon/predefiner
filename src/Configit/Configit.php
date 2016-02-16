ff7569a99fe071509431908efa4623c9e67a54c6
Gavin G Gordon <gmarkowski@my.bcit.ca>
2016-02-16 01:53:46 -0800

    Updated Configit.php
    Updated Configit.php with completed PHP class.

---
+++ src/Configit/Configit.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Configit;
+
+class Configit
+{
+
+	private $userappconfig;
+	private $configarray;
+	
+	public function __construct()
+	{
+		if( file_exists( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'user_app_config.php' ) ) {
+			$this->userappconfig = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'user_app_config.php';
+		}
+		//$this->configarray = array();
+		return $this;
+	}
+	
+	public function init( $settings = array() )
+	{
+		$configarray = require( $this->userappconfig );
+		$this->configarray = array_merge( $configarray, $settings );
+		if( $this->build() ) {
+			return $this->userappconfig;
+		}
+	}
+	
+	private function build()
+	{
+		$newfile = '&lt;?php ' . "\n\n" . 'return array(' . "\n";
+		foreach( $this->configarray as $index => $value ) {
+			$newfile .= '&apos;' . strtoupper( $index ) . '&apos; => &apos;' . $value . '&apos;' . "\n";
+		}
+		$newfile .= ');' . "\n";
+		if( file_put_contents( $this->userappconfig, html_entity_decode( $newfile ) ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}
