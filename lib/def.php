128a4375352c704a25fb9752a03e055c053cb769
Gavin G Gordon <gmarkowski@my.bcit.ca>
2016-02-22 16:28:22 -0800

    Updated composer.json and def.php
    Updated composer.json and def.php to reflect new class name.

--- lib/def.php
+++ lib/def.php
@@ -1,11 +1,11 @@
 <?php
 
-include_once( __DIR__ . DIRECTORY_SEPARATOR . 'configit_helper.php' );
+include_once( __DIR__ . DIRECTORY_SEPARATOR . 'predefiner_helper.php' );
 
 function def( $settings_array_file )
 {
 	$settings_array = require( $settings_array_file );
-	if( configit_helper( $settings_array ) ) {
+	if( predefiner_helper( $settings_array ) ) {
 		return true;
 	} else {
 		return false;
