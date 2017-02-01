4ee741261d4ea5d8c2f3bfbe60cecf247297eace
Gavin Gordon <me@gavingordon.com>
2017-02-01 01:13:56 -0800

    Fixed major error
    Fixed a major error in the 'init' method, where an unknown function was
    being referenced.

--- src/Predefiner.php
+++ src/Predefiner.php
@@ -11,11 +11,9 @@ class Predefiner
 	public function __construct()
 	{
 		$file = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'user_app_config.php';
-		if(! file_exists( $file ) ) {
-        	$template = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'user_app_configTemplate.php';
-			@copy( $template, $file );
-		}
-		$this->userappconfig = $file;
+       $template = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'user_app_configTemplate.php';
+       @copy( $template, $file );
+       $this->userappconfig = $file;
 		return $this;
 	}
 	
@@ -37,21 +35,37 @@ class Predefiner
 		$newfile = '<?php' . "\n\n" . 'return array(' . "\n\t";
 		foreach( $configarray as $index => $value ) {
           $strend = ( $i == count( $this->configarray ) - 1 ) ? "\n" : ",\n\t";
-			$const = strtoupper( $index );
-			$newfile .= "'" . $const . "' => '" . $value . "'" . $strend;
+			$const = preg_replace( '/[^a-z0-9\_]+?/i', '_', strtoupper( $index ) );
+			$newfile .= "'{$const}' => '{$value}'" . $strend;
           $i++;
 		}
 		$newfile .= ");" . "\n\n";
-		if( file_put_contents( $this->userappconfig, $newfile ) ) {
+      if( file_put_contents( $this->userappconfig, $newfile ) ) 
+      {
 			return true;
 		} else {
 			return false;
 		}
 	}
+
+   private function define_many( $constants )
+   {
+     if( is_array( $constants ) && count( $constants ) >= 1 )
+     {
+       foreach( $constants as $constant => $definition )
+       {
+         if(! defined( $constant ) )
+         {
+           define( $constant, $definition );
+         }
+       }
+     }
+   }
 	
 	public function init()
 	{
-		def( $this->userappconfig );
+      $constants = require( $this->userappconfig );
+      $this->define_many( $constants );
 	}
 	
 }
